fcfae69e4e094d187b10307c285b72b83f91ebe8
MateusFerraz <mateus.p.ferraz@gmail.com>
2023-08-25 21:04:24 -0300

    Subindo arquivos

---
+++ Tratamento de exceçoes/TryCatch/src/main/java/com/mycompany/trycatch/TryCatch.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trycatch;
+
+/**
+ *
+ * @author Mateus
+ */
+public class TryCatch {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
